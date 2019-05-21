d0cb9691ec44ec2c0c3130e0d5bc5feb545f591d
Leonardo Anjos <leodrums11@gmail.com>
2019-05-21 18:22:04 -0300

    lib to charts

--- src/components/Grafico/Barra1.jsx
+++ src/components/Grafico/Barra1.jsx
@@ -1,40 +1,60 @@
 import React, { Component } from 'react';
-import { Chart } from 'react-google-charts';
+import { connect } from 'react-redux';
+import { getAll } from './Redux'
 
-const dataSource = [
-  [
-    'Orgao',
-    'Total',
-    { role: 'style' },
-    {
-      sourceColumn: 0,
-      role: 'annotation',
-      type: 'string',
-      calc: 'stringify',
-    },
-  ],
-  ['SERPRO', 24, '#F25764', null],
-  ['Min. Planejamento', 76, '#D8DCD8', null],
-  ['MEC', 64, '#409EEC', null],
-  ['DATAPREV', 52, '#54BF64', null],
-  ['Uber', 9, '#F29727', null] 
-]
+// import { Chart } from 'react-google-charts';
 
-export class GraficoBarra1 extends Component {
-  render() {
-    return(
-      <Chart
-        chartType="BarChart"
-        loader={<div>Loading Chart</div>}
-        data={dataSource}
-        options={{
-          title: 'Média de Requisições',
-          width: 600,
-          height: 400,
-          bar: { groupWidth: '70%' },
-          legend: { position: 'none' },
-        }}
-      />
-    );
-  }
-}
+// const dataSource = [
+//   [
+//     'Orgao',
+//     'Total',
+//     { role: 'style' },
+//     {
+//       sourceColumn: 0,
+//       role: 'annotation',
+//       type: 'string',
+//       calc: 'stringify',
+//     },
+//   ],
+//   ['SERPRO', 24, '#F25764', null],
+//   ['Min. Planejamento', 76, '#D8DCD8', null],
+//   ['MEC', 64, '#409EEC', null],
+//   ['DATAPREV', 52, '#54BF64', null],
+//   ['Uber', 9, '#F29727', null] 
+// ]
+
+export const GraficoBarra1 =
+
+connect(
+  ({ alldata }) => ({ alldata }),
+  { getAll }
+)(
+  class extends Component {
+
+    render() {
+      
+      return(
+        // <Chart
+        //   chartType="BarChart"
+        //   loader={<div>Loading Chart</div>}
+        //   // data={dataSource}
+        //   options={{
+        //     title: 'Média de Requisições',
+        //     width: 600,
+        //     height: 400,
+        //     bar: { groupWidth: '70%' },
+        //     legend: { position: 'none' },
+        //   }}
+        // />
+        <div>GraficoBarra1</div>
+      );
+
+    }
+
+    componentDidMount() {
+      this.props.getAll();
+    }
+
+  } // class
+
+); // redux
