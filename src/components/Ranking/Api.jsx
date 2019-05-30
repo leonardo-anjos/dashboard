7672ffb1eb373711055e7b45682cfd82b5e516c5
Leonardo Anjos <leodrums11@gmail.com>
2019-05-30 17:42:17 -0300

    treeMap graphic 2

--- src/components/Ranking/Api.jsx
+++ src/components/Ranking/Api.jsx
@@ -7,42 +7,6 @@ import {Chart} from 'react-google-charts';
 
 const Option = Select.Option;
 
-const dataSource = [
-  [
-    'Location',
-    'Parent',
-    'Market trade volume (size)',
-    'Market increase/decrease (color)',
-  ],
-  ['Global', null, 0, 0],
-  ['America', 'Global', 0, 0],
-  ['Europe', 'Global', 0, 0],
-  ['Asia', 'Global', 0, 0],
-  ['Australia', 'Global', 0, 0],
-  ['Africa', 'Global', 0, 0],
-  ['Brazil', 'America', 11, 10],
-  ['USA', 'America', 52, 31],
-  ['Mexico', 'America', 24, 12],
-  ['Canada', 'America', 16, -23],
-  ['France', 'Europe', 42, -11],
-  ['Germany', 'Europe', 31, -2],
-  ['Sweden', 'Europe', 22, -13],
-  ['Italy', 'Europe', 17, 4],
-  ['UK', 'Europe', 21, -5],
-  ['China', 'Asia', 36, 4],
-  ['Japan', 'Asia', 20, -12],
-  ['India', 'Asia', 40, 63],
-  ['Laos', 'Asia', 4, 34],
-  ['Mongolia', 'Asia', 1, -5],
-  ['Iran', 'Asia', 18, 13],
-  ['Pakistan', 'Asia', 11, -52],
-  ['Egypt', 'Africa', 21, 0],
-  ['S. Africa', 'Africa', 30, 43],
-  ['Sudan', 'Africa', 12, 2],
-  ['Congo', 'Africa', 10, 12],
-  ['Zaire', 'Africa', 8, 10]
-]
-
 export const RankingApi =
 
 // connect(
@@ -78,23 +42,15 @@ export const RankingApi =
             </Col>
           </Row>
 
+          {/* graphic */}
           <Chart
             style={{ margin: "20px auto 0", padding: "0 20px" }}
             height={'300px'}
             chartType="TreeMap"
             loader={<div>Loading Chart</div>}
-            data={dataSource}
-            options={{
-              minColor: '#f00',
-              midColor: '#ddd',
-              maxColor: '#0d0',
-              headerHeight: 15,
-              fontColor: 'black',
-              showScale: true,
-            }}
+            data={result}
             rootProps={{ 'data-testid': '1' }}
           />
-
         </React.Fragment>
       );
     }
@@ -106,9 +62,11 @@ export const RankingApi =
 
     getData = async () => {
       const response = await index.post('_xpack/sql?format=json', {
-        "query": "select Estado as estado, sum(Numero_Patrimonial) as total from data where Filial = 'FLA' group by estado order by total desc" 
+        "query": "select Orgao as org, Superintendencia sup, count(Numero_Patrimonial) as cnt from data where Filial = 'FLA' and sup = 'SUPOP' group by sup, org" 
       });
 
+      response.data.rows[0] = ["orgao", "superintendencia", "total"];
+      response.data.rows[1] = ["SUPOP", null, 0];
       this.setState({ result: response.data.rows });
     }
   
